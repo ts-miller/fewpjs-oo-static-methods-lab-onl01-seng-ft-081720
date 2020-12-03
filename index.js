81907aced655049b6de79344a1e4174d465b9059
ts-miller <tylerstephen11@gmail.com>
2020-12-03 13:04:38 -0500

    commit tree 247e16952f0dcabefb839fbc7abf199bda86ca25

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
+  static sanitize(s) {
+    const exceptions = ["-", "'", " "]
+    return s.replace(/[^a-z0-9+\s\'\-]+/gi, '')
+  }
+
+  static titleize(s) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'and', 'from']
+    return s.split(' ').map((word, i) => {
+      if ((!exceptions.includes(word)) || i === 0 ) {
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(' ')
+  }
 }
